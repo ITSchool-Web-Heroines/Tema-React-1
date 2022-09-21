05c711585c16c549869d422610d62f9c7979bd88
Semida <semidag@outlook.com>
2022-09-21 23:05:32 +0300

    option2

--- App.js
+++ App.js
@@ -24,7 +24,9 @@ Folosește React pentru a afișa următorul cod în elementul cu ID #app
 </div>
 */
 
-const listgroup = [
+import "./styles.css";
+export default function App() {
+  const products = [
     "Pâine",
     "Lapte",
     "Ciocolată",
@@ -32,19 +34,23 @@ const listgroup = [
     "Miere",
     "Mandarine"
   ];
-  export default function App() {
-    return (
-      <div className="App">
-        <h1>Tema React </h1>
-        <span>
-          <h2> Lista de cumparaturi</h2>{" "}
-        </span>
-        <ul>
-            {listgroup.map((cumparaturi) => (
-              <li> {cumparaturi} </li>
-            ))}
-          </ul>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="vh-100 d-flex flex-column">
+      <header className="navbar bg-primary bg-gradient">
+        <div className="container-fluid">
+          <h1 className="navbar-brand mb-0 text-white">Tema React</h1>
+        </div>
+      </header>
+      <main className="container my-5 flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+        <ul className="products">
+          <li className="shopping">
+            <span className="fw-bold">Lista de cumpărături</span>
+          </li>
+          {products.map((shopping) => (
+            <li key={shopping}> {shopping} </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
+}
