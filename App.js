65fea4d0a21d8fd6e74adee2b72e9761a0587232
Belei Bianca <biebianca99@gmail.com>
2022-09-22 12:29:01 +0300

    Tema

--- App.js
+++ App.js
@@ -22,9 +22,27 @@ Folosește React pentru a afișa următorul cod în elementul cu ID #app
     </main>
 </div>
 */
-
+const listItems = ["Paine", "Lapte", "Ciocolata", "Mere", "Miere", "Mandarine"];
 export default function App() {
-    return (
-        <div></div>
-    )
+  return (
+    <div className="vh-100 d-flex flex-column">
+      <header className="navbar bg-primary bg-gradient">
+        <div className="container-fluid">
+          <h1 className="navbar-brand mb-0 text-white">Tema React</h1>
+        </div>
+      </header>
+      <main className="container my-5 flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+        <ul className="list-group">
+          <li className="list-group-item">
+            <span className="fw-bold">Lista de cumparaturi</span>
+          </li>
+          {listItems.map((item) => (
+            <li className="list-group-item" key={item}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
 }
