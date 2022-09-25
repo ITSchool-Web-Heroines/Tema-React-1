ac56a0261d99298b9b1e640c4052b0e67d4c421a
anamariap21 <anapacurar321@gmail.com>
2022-09-25 18:04:53 +0300

    React 1

--- App.js
+++ App.js
@@ -23,8 +23,30 @@ Folosește React pentru a afișa următorul cod în elementul cu ID #app
 </div>
 */
 
+const ingredients = ["Pâine", "Lapte", "Ciocolată", "Miere", "Mere", "Mandarine"];
+
 export default function App() {
     return (
-        <div></div>
+        <div>
+            <div className="vh-100 d-flex flex-column">
+                <header className="navbar bg-primary bg-gradient">
+                    <div className="container-fluid">
+                        <h1 className="navbar-brand mb-0 text-white">Tema React Pacurar</h1>
+                    </div>
+                </header>
+                <main className="container my-5 flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+                    <ul className="list-group">
+                        <li className="list-group-item">
+                            <span className="fw-bold">Lista de cumpărături</span>
+                        </li>
+                        {ingredients.map((ingredient) => (
+                            <li className="list-group-item" key={ingredient}>
+                                {ingredient}
+                            </li>
+                        ))}
+                    </ul>
+                </main>
+            </div>
+        </div>
     )
 }
